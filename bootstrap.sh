16d55d411eb15230d871cbcc0efe6faba48a5eab
Joseph Briggs <jbriggsr3@gmail.com>
2017-01-17 16:08:08 +0000

    Minor changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
+#!/
 
-ANDROID_SDK_FILENAME=android-sdk_r24.2-linux.tgz
-ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
+ANDROID_SDK_FILENAME=tools_r25.2.3-linux.zip
+ANDROID_SDK=https://dl.google.com/android/repository/$ANDROID_SDK_FILENAME
 
 #sudo apt-get install python-software-properties
-#sudo add-apt-repository ppa:webupd8team/java
-apt-get update
-apt-get install -y npm git openjdk-7-jdk ant expect
-npm install -g n
-n stable
+sudo add-apt-repository ppa:openjdk-r/ppa
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+apt-get update && apt-get -y upgrade
+apt-get install -y nodejs unzip git openjdk-8-jre ant expect lib32z1 lib32ncurses5 lib32bz2-1.0
 
 curl -O $ANDROID_SDK
-tar -xzvf $ANDROID_SDK_FILENAME
+mkdir android-sdk-linux/
+sudo unzip $ANDROID_SDK_FILENAME -d android-sdk-linux/
 sudo chown -R vagrant android-sdk-linux/
 
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386" >> /home/vagrant/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
 
-npm install -g cordova
-npm install -g ionic
+sudo npm install -g ionic cordova
+
 expect -c '
 set timeout -1   ;
 spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,build-tools-22.0.1
-expect { 
+expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
